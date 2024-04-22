7ef034b41e2d9d9d811d5259d9d139afebd79ae8
Igor <foreverigor@users.noreply.github.com>
2024-04-22 23:30:05 +0200

    Fix ReportParser

--- src/main/java/org/dpytel/intellij/plugin/maventest/ReportParser.java
+++ src/main/java/org/dpytel/intellij/plugin/maventest/ReportParser.java
@@ -18,6 +18,7 @@ package org.dpytel.intellij.plugin.maventest;
 
 import com.intellij.execution.testframework.sm.runner.GeneralTestEventsProcessor;
 import com.intellij.execution.testframework.sm.runner.events.*;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import jetbrains.buildServer.messages.serviceMessages.TestIgnored;
@@ -29,12 +30,13 @@ import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
 public class ReportParser {
+    private static final Logger logger = Logger.getInstance(ReportParser.class);
+
     private final GeneralTestEventsProcessor testEventsProcessor;
 
     public ReportParser(GeneralTestEventsProcessor testEventsProcessor) {
@@ -43,17 +45,13 @@ public class ReportParser {
 
     public void parseTestSuite(VirtualFile child) throws IOException, JDOMException {
         SAXBuilder builder = new SAXBuilder();
-        File file = new File(child.getCanonicalPath());
-        FileInputStream in = null;
-        try {
-            in = new FileInputStream(file);
+        try (FileInputStream in = new FileInputStream(child.getCanonicalPath())) {
             Document document = builder.build(in);
             Element rootElement = document.getRootElement();
             Attribute nameAttribute = rootElement.getAttribute("name");
             String name = nameAttribute.getValue();
             TestSuiteStarted suiteStarted = new TestSuiteStarted(name);
             testEventsProcessor.onSuiteStarted(new TestSuiteStartedEvent(suiteStarted, null));
-            @SuppressWarnings("unchecked")
             List<Element> testcases = rootElement.getChildren("testcase");
 
             for (Element testcase : testcases) {
@@ -61,75 +59,64 @@ public class ReportParser {
             }
 
             testEventsProcessor.onSuiteFinished(new TestSuiteFinishedEvent(name));
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    // ignore
-                }
-            }
         }
     }
 
     private void parseTestCase(Element testcase) {
         String methodName = testcase.getAttributeValue("name");
-        String className = testcase.getAttributeValue("classname");
 
-        TestStarted testStarted = new TestStarted(methodName, true, null);
-        TestStartedEvent testStartedEvent = new TestStartedEvent(testStarted, null);
-        testEventsProcessor.onTestStarted(testStartedEvent);
+        testEventsProcessor.onTestStarted(new TestStartedEvent(new TestStarted(methodName, true, null), null));
 
-        List children = testcase.getChildren();
         Long duration = getDuration(testcase);
-        if (children.size() == 0) {
-            reportTestFinished(methodName, duration);
-        } else {
-            Element errorOrFailure = (Element) children.get(0);
-            String stateName = errorOrFailure.getName();
-            if ("skipped".equals(stateName)) {
-                String message = errorOrFailure.getAttributeValue("message", "");
-                testEventsProcessor.onTestIgnored(new TestIgnoredEvent(new TestIgnored(methodName, message), null));
-            } else {
-                StringBuilder sysout = new StringBuilder();
-                StringBuilder syserr = new StringBuilder();
-                for (int i = 1; i < children.size(); ++i) {
-                    Element child = (Element) children.get(i);
-                    String name = child.getName();
-                    String text = child.getText();
-                    if ("system-out".equals(name)) {
-                        sysout.append(text);
-                    } else if ("system-err".equals(name)) {
-                        syserr.append(text);
-                    }
+        if (!testcase.getChildren().isEmpty()) {
+            StringBuilder sysOut = new StringBuilder();
+            StringBuilder sysErr = new StringBuilder();
+
+            Runnable report = null;
+
+            for (Element child : testcase.getChildren()) {
+                String message = child.getAttributeValue("message", "");
+                switch (child.getName()) {
+                    case "skipped":
+                        report = () -> testEventsProcessor.onTestIgnored(new TestIgnoredEvent(new TestIgnored(methodName, message), null));
+                        continue;
+                    case "failure":
+                        report = () -> reportTestFailure(methodName, false, message, child.getText());
+                        continue;
+                    case "error":
+                        report = () -> reportTestFailure(methodName, true, message, child.getText());
+                        continue;
+                    case "system-out":
+                        sysOut.append(child.getText());
+                        continue;
+                    case "system-err":
+                        sysErr.append(child.getText());
+                        continue;
+                    default:
+                        logger.warn("unknown testcase property '" + child.getName() + "'");
                 }
-                if (sysout.length() > 0) {
-                    testEventsProcessor.onTestOutput(new TestOutputEvent(methodName, sysout.toString(), true));
-                }
-                if (syserr.length() > 0) {
-                    testEventsProcessor.onTestOutput(new TestOutputEvent(methodName, syserr.toString(), false));
-                }
-                boolean testError;
-                if ("failure".equals(stateName)) {
-                    testError = false;
-                } else if ("error".equals(stateName)) {
-                    testError = true;
-                } else {
-                    throw new IllegalStateException("Unknown state: " + stateName);
-                }
-                String failureMessage = errorOrFailure.getAttributeValue("message", "");
-                reportTestFailure(methodName, testError, failureMessage);
-                reportTestFinished(methodName, duration);
+            }
+
+            if (sysOut.length() > 0) {
+                testEventsProcessor.onTestOutput(new TestOutputEvent(methodName, sysOut.toString(), true));
+            }
+            if (sysErr.length() > 0) {
+                testEventsProcessor.onTestOutput(new TestOutputEvent(methodName, sysErr.toString(), false));
+            }
+
+            if (null != report) {
+                report.run();
             }
         }
+        reportTestFinished(methodName, duration);
     }
 
     private void reportTestFinished(String methodName, Long duration) {
         testEventsProcessor.onTestFinished(new TestFinishedEvent(methodName, duration));
     }
 
-    private void reportTestFailure(String methodName, boolean testError, String failureMessage) {
-        testEventsProcessor.onTestFailure(new TestFailedEvent(methodName, failureMessage, null, testError, null, null));
+    private void reportTestFailure(String methodName, boolean testError, String failureMessage, String stackTrace) {
+        testEventsProcessor.onTestFailure(new TestFailedEvent(methodName, failureMessage, stackTrace, testError, null, null));
     }
 
     private Long getDuration(Element testcase) {
